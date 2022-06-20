5404639a17943403e40e894efcfca6436e16651e
Nihar Ranjan Hota <85618121+Nihar1420@users.noreply.github.com>
2022-06-20 22:20:06 +0530

    Update Footer.jsx

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -20,7 +20,7 @@ const Footer = () => {
 
       <div className="footer_socials">
         <a href="https://www.linkedin.com/feed/" target="_blank" rel="noopener noreferrer"><BsLinkedin /></a>
-        <a href="https://github.com/" target="_blank" rel="noopener noreferrer"><BsGithub /></a>
+        <a href="https://github.com/Nihar1420/" target="_blank" rel="noopener noreferrer"><BsGithub /></a>
         <a href="https://www.hackerrank.com/niharranjanhota2" target="_blank" rel="noopener noreferrer"><SiHackerrank /></a>
       </div>
 
